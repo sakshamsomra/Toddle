d26eb8f624eab4cf2f7bfd68aa2254d8be258ab8
sakshamsomra <77576187+sakshamsomra@users.noreply.github.com>
2023-07-21 15:21:28 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -55,7 +55,7 @@ export default function Navbar() {
 
                 <div class="navhold">
                     <Link to={"/sign"}><div id="navitem"> <img src={add}></img> SignIn</div></Link>
-                    <Link ><button id="navitem" onClick={log}> <img src={login}></img> LogOut</button></Link>
+                    <Link ><button id="navitem" onClick={log} className="invisible-button"> <img src={login}></img> LogOut</button></Link>
                     <Link to={"/write"}><div id="navitem"><img src={edit}></img>Start Writing</div></Link>
                     <Link to={"/myinfo"}><div id="navitem"><img src={userp}></img>My Info</div></Link>
                 </div>

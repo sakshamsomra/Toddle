e5ada4693fd47fa6c390a057ac34511deff34ddb
sakshamsomra <77576187+sakshamsomra@users.noreply.github.com>
2023-08-12 01:19:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const ejs = require('ejs');
 const cors = require('cors');
@@ -6,7 +8,6 @@ const session = require('express-session')
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
-const { query } = require('express');
 const saltRounds = 10;
 const path = require('path')
 const multer = require("multer");
@@ -14,26 +15,26 @@ const cookieParser = require('cookie-parser');
 const MySQLStore = require('express-mysql-session')(session);
 // app.use('/Images', express.static('Images'));
 
-const emailValidator = require('email-validator');
-
-const isValidEmail = emailValidator.validate('example@email.com');
-
 const sessionStore = new MySQLStore({
   host: 'db4free.net',
-  user: 'puneetsomra',
-  password: 'hello123',
-  database: 'pblogger',
+  user: 'somrasaksham',
+  password: 'Rengoku1@',
+  database: 'jeweldata',
 });
+
+
 
 
 
 app.use(bodyParser.json());
 app.use(express.json());
+
 app.use(cors({
-  origin: ["https://blog-project-client.vercel.app"],
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
-}));
+})); 
+
 
 
 
@@ -57,27 +58,25 @@ app.use(session({
   store: sessionStore,
 
 }));
-
-
-
+   
 
 const conn = mysql.createConnection({
   host: 'db4free.net',
-  user: 'puneetsomra',
-  password: 'hello123',
-  database: 'pblogger',
+  user: 'somrasaksham', 
+  password: 'Rengoku1@',
+  database: 'jeweldata',
   port: '3306',
   insecureAuth : true
+
 });
 
-
 conn.connect((err) => {
-  if (err) throw err;
+  if (err) throw err; 
   console.log('Mysql Connected with App...');
 });
 
 
-const user = "";
+
 
 
 const storage = multer.diskStorage({
@@ -88,179 +87,295 @@ const storage = multer.diskStorage({
     cb(null, file.originalname)
   }
 });
-
+ 
 const upload = multer({ storage: storage });
 
-
-
+ 
+ 
 
 app.get('/api/get', (req, res) => {
-  let sqlQuery = "SELECT * FROM blogs";
+  let sqlQuery = "SELECT * FROM items";
 
   let query = conn.query(sqlQuery, (err, results) => {
-    if (err) throw err;
+    
     res.send(results);
   });
-});
+}); 
 
-
-app.get('/api/myfeed', (req, res) => {
-  let sqlQuery = `SELECT * FROM blogs WHERE name = '${req.session.user}'`;
-
-  let query = conn.query(sqlQuery, (err, results) => {
-    if (err) throw err;
-    res.send(results);
-  });
-});
-
+ 
 
 app.get('/api/getcart', (req, res) => {
-  let sqlQuery = "SELECT * FROM cart";
+  let sqlQuery = `SELECT * FROM cart WHERE name = '${req.session.user}'`;
 
   let query = conn.query(sqlQuery, (err, results) => {
-    if (err) throw err;
+    
     res.send(results);
   });
 });
 
+ 
 
+app.get('/api/getprice', (req, res) => {
+  let sqlQuery = `SELECT (price * qty) AS totalPrice FROM cart WHERE name = '${req.session.user}'`;
+  
+ 
+  let query = conn.query(sqlQuery, (err, results) => {
+      
+      res.send(results);
+      console.log(results);
+      
+  });
+});   
+  
 
+  
+app.put("/api/inc", upload.single('file'), (req, res) => {
+  const id = req.body.id; 
 
+  let sqlQuery = `UPDATE cart SET qty = qty + 1 WHERE id = ${id}`;
 
-
-
-
-app.post("/api/blog", upload.single('file'), (req, res) => {
-  let data = { title: req.body.title, body: req.body.body, name: req.body.name, date: req.body.date, likes: req.body.likes };
-
-  let sqlQuery = "INSERT INTO blogs SET ?";
-
-  let query = conn.query(sqlQuery, data, (err, results) => {
-    if (err) throw err;
+  let query = conn.query(sqlQuery, id, (err, results) => {
+    if (err) throw err; 
     res.send(apires(results));
   });
 
-  console.log(req.body);
+  console.log(req.body); 
 });
 
 
-app.post("/api/likes", upload.single('file'), (req, res) => {
-  let id = req.body.id;
 
-  let sqlQuery = `UPDATE blogs SET likes = likes + 1 WHERE id = ${id}`;
+
+app.put("/api/dec", upload.single('file'), (req, res) => {
+  const id = req.body.id; 
+
+  let sqlQuery = `UPDATE cart SET qty = qty - 1 WHERE id = ${id}`;
+
+  let query = conn.query(sqlQuery, id, (err, results) => {
+    if (err) throw err; 
+    res.send(apires(results));
+  });
+
+  console.log(req.body); 
+});
+
+app.post("/api/dec", upload.single('file'), (req, res) => {
+  const id = req.body.id; 
+
+  let sqlQuery = `DELETE FROM cart where qty = 0`;
+
+  let query = conn.query(sqlQuery, id, (err, results) => {
+    if (err) throw err; 
+    res.send(apires(results));
+  });
+  
+  console.log(req.body); 
+});  
+
+
+
+app.post("/api/order", upload.single('file'), (req, res) => {
+  
+
+  let sqlQuery = `SELECT name, prod_name, qty FROM cart WHERE name = '${req.session.user}'`;
 
   let query = conn.query(sqlQuery, (err, results) => {
-    if (err) throw err;
-    res.send(apires(results));
-  });
-
-  console.log(req.body);
-});
-
-
-
-app.post("/api/users", upload.single('file'), (req, res) => {
-  let data = { email: req.body.email, username: req.body.username, password: req.body.password };
-
-  let sqlQuery = "INSERT INTO users SET ?";
-
-  let query = conn.query(sqlQuery, data, (err, results) => {
-    if (err) throw err;
-
-
+    if (err) throw err; 
     res.send(apires(results));
 
+    const products = results.map((row) => [row.name, row.prod_name, row.qty]);
 
+    const OrderQuery = 'INSERT INTO `orders`(name, prod_name, qty) VALUES ?';
+
+    conn.query(OrderQuery, [products], (error, result) => {
+      if (err) throw err; 
+
+      console.log('Data inserted into order table successfully');
+      console.log(products); 
+    })
   });
 
-  console.log(req.body);
-});
+  
+  console.log(req.body);   
+});  
+   
 
+app.delete("/api/order", upload.single('file'), (req,res) => {
 
-app.post("/api/addcart", (req, res) => {
-  let data = { name: req.body.name, price: req.body.price };
+  let sqlQuery = `DELETE FROM cart WHERE name = '${req.session.user}'`;
 
-  let qty = req.body.qty;
-  let id = req.body.id;
+  conn.query(sqlQuery, (err, results) => {
 
-  let sqlQuery = "INSERT INTO cart SET ?";
-
-
-  let query = conn.query(sqlQuery, data, (err, results) => {
-    if (err) throw err;
-    res.send(apires(results));
-  });
-
-  console.log(req.body);
-});
-
-
-
-// app.get("/api/search", (req, res) => { 
-//   const searchterm = req.query.searchterm;  
-
-//   let sqlQuery = `SELECT * FROM blogs WHERE name LIKE '%${searchterm}%'`;
-
-
-
-//   let query = conn.query(sqlQuery, data, (err, results) => {
-//     if (err) throw err;  
-//     res.send(apires(results));     
-//   });  
-
-//   console.log(search);    
-// });  
-
-
-
-
-
-
-app.get("/api/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.send({ loggedIn: true, user: req.session.user })
-  } else {
-    res.send({ loggedIn: false })
-  }
-
+  })
 
 })
+ 
 
-app.post("/api/login", upload.single('file'), (req, res) => {
+
+
+app.put("/api/order", upload.single('file'), (req, res) => {
+  const username = req.session.user;
+
+  let cartQuery = `SELECT prod_name, qty FROM cart WHERE name = ?`;
+
+  conn.query(cartQuery, [username], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error retrieving cart items" });
+    }
+
+    const products = results;
+
+    // Update items in the items table based on the cart items
+    const updatePromises = products.map((product) => {
+      const removeQuery = `UPDATE items SET qty = qty - ? WHERE prod_name = ?`;
+      const { qty, prod_name } = product;
+
+      return new Promise((resolve, reject) => {
+        conn.query(removeQuery, [qty, prod_name], (err, result) => {
+          if (err) {
+            console.error(`Error updating quantity for ${prod_name}`);
+            reject(err);
+          } else {
+            console.log(`Quantity updated for ${prod_name}`);
+            resolve();
+          }
+        });
+      });
+    });
+
+    Promise.all(updatePromises)
+      .then(() => {
+        console.log("All items updated successfully");
+        res.status(200).json({ message: "Items removed from cart" });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ error: "Error updating quantities" });
+      });
+  });
+});
+
+
+
+
+
+  
+
+
+
+app.post("/api/items", upload.single('file'), (req, res) => {
+  let data = { prod_name: req.body.prod_name, category: req.body.category, description: req.body.description, price: req.body.price, saleprice: req.body.saleprice, qty: req.body.qty };
+
+  let sqlQuery = "INSERT INTO categories SET ?";
+
+  let query = conn.query(sqlQuery, data, (err, results) => {
+    if (err) throw err;
+    res.send(apires(results));
+  });
+
+  console.log(req.body); 
+});
+
+
+
+app.post("/api/users",upload.single('file'), (req, res) => {
+  let data = { email: req.body.email, username: req.body.username, password: req.body.password };
+
+  let sqlQuery = "INSERT INTO user SET ?";
+ 
+  let query = conn.query(sqlQuery, data, (err, results) => {
+    if (err) throw err;
+
+   
+    res.send(apires(results));
+    
+    
+  });
+ 
+  console.log(req.body);
+});
+
+ 
+app.post("/api/addcart",upload.single('file'), (req, res) => {
+  let data = { name: req.body.name, prod_name: req.body.prod_name,description: req.body.description, price: req.body.price, qty: req.body.qty};
+  let id = req.body.id;
+
+  let sqlQuery = "INSERT INTO cart SET ?" ;
+  
+  let query = conn.query(sqlQuery, data, (err, results) => {
+   
+    res.send(apires(results)); 
+  });
+
+  
+});
+
+
+// app.put("/api/addcart",upload.single('file'), (req, res) => {
+  
+//   let id = req.body.id;
+
+//   let sqlQuery1 = `UPDATE items SET qty = qty - 1 WHERE id = ${id}`;
+
+//   let query1 = conn.query(sqlQuery1, (err, results) => {
+    
+   
+//   });
+ 
+ 
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+app.get("/api/login", (req,res) => {
+  if(req.session.loggedIn){
+    res.send({loggedIn: true, user: req.session.user})
+  } else{
+    res.send({loggedIn: false})
+  }
+})
+
+
+app.post("/api/login",upload.single('file'), (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
 
-
-
-  conn.query("SELECT * FROM users WHERE username = ? AND password = ?",
-    [username, password],
-    (err, results) => {
-      if (err) {
-        console.log(err);
-      } else {
-        if (results.length > 0) {
-          req.session.loggedIn = true;
-          req.session.user = username;
-          console.log(req.session.user);
-          res.send(results);
-        } else {
-          res.send({ message: "Wrong Combination" });
-        }
+  conn.query("SELECT * FROM user WHERE username = ? AND password = ?", 
+  [username, password], 
+  (err, results) => {
+    if (err) {
+      console.log(err);
+    }else{
+      if(results.length > 0){
+        req.session.loggedIn = true;
+        req.session.user = username;
+        console.log(req.session.user);
+        res.send(results);
+      } else{
+        res.send({message: "Wrong Combination"});
       }
+    }
+    
 
-
-    });
+  });
 
   console.log(req.body);
 });
 
 
-app.post("/api/logout", upload.single('file'), (req, res) => {
-
-
-  req.session.destroy(function (err) {
-    if (err) {
+app.post("/api/logout",upload.single('file'), (req,res) => {
+  req.session.destroy(function(err) {
+    if(err) {
       console.log(err);
       res.status(500).send('An error occurred while logging out.');
     } else {
@@ -271,31 +386,13 @@ app.post("/api/logout", upload.single('file'), (req, res) => {
 })
 
 
-app.post('/api/delete/:id', (req, res) => {
-  const id = req.params.id;
-
-  const sqlQuery = 'DELETE FROM blogs WHERE id = ?';
-  conn.query(sqlQuery, [id], (err, result) => {
-    if (err) {
-      console.error('Error deleting row:', err);
-      res.status(500).json({ error: 'Error deleting row' });
-    } else {
-      res.send({ message: 'topic deleted successfully' });
-      console.log("deleted")
-    }
-  });
-});
-
-
-
-
 app.get('/api/username', (req, res) => {
   res.send(req.session.user);
 });
 
+ 
 
-
-
+  
 
 function apires(results) {
   return JSON.stringify({ "status": 200, "error": null, "res": results });
@@ -307,5 +404,5 @@ Server listening
 --------------------------------------------
 --------------------------------------------*/
 
-
-app.listen(3001);
+ 
+app.listen(3001);   

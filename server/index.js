5e4c9dfefbce684128b042558dccc20debdd73c1
sakshamsomra <77576187+sakshamsomra@users.noreply.github.com>
2023-08-12 01:46:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["https://toddle-one.vercel.app"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true
 })); 

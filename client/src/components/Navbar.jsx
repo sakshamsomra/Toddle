8100674e043178acae823fb92e335f7953c59829
sakshamsomra <77576187+sakshamsomra@users.noreply.github.com>
2023-08-12 02:19:29 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -62,7 +62,7 @@ export default function Navbar() {
                             </li>
                             <li class="nav-item">
 
-                                <Link ><button  onClick={log} id="logout"  class="nav-link" >Log Out</button></Link>
+                                <button  onClick={log} id="logout"  class="nav-link" >Log Out</button>
                             </li>
                             <li class="nav-item">
 

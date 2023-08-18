882ee65253d3369c86f123aa33a6313ab205b12a
sakshamsomra <77576187+sakshamsomra@users.noreply.github.com>
2023-08-18 22:22:26 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import './Slider.css'
 import './Checkout.css'
 import './display.css'
 import './loader.css'
+import './contact.css'
 import Landing from './components/Landing';
 import Navbar from './components/Navbar';
 import Home from './components/Home';

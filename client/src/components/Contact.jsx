444ead88044b8e3af2e7fa305647706d5c15a443
sakshamsomra <77576187+sakshamsomra@users.noreply.github.com>
2023-08-18 22:18:48 +0530

    Add files via upload

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import Navbar from "./Navbar";
+import axios from "axios";
+
+export default function Contact() {
+
+    axios.defaults.withCredentials = true;
+
+
+
+    return (
+        <>
+
+             <Navbar />
+
+
+            <div class="card">
+                <div class="infos">
+                    <div class="image"></div>
+                    <div class="info">
+                        <div>
+                            <p class="name">
+                                Saksham
+                            </p>
+                            <p class="function">
+                                developer
+                            </p>
+                        </div>
+                        <div class="stats">
+                            <p class="flex flex-col">
+                                E-Mail
+                                <span class="state-value">
+                                    saksham.somra@gmail.com
+                                </span>
+                            </p>
+                            <p class="flex">
+                                P.no
+                                <span class="state-value">
+                                    +91 9646486947
+                                </span>
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
+
+
+        </>
+    )
+}

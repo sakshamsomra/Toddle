9e4698506d1586873b1253d2830973596bf93324
sakshamsomra <77576187+sakshamsomra@users.noreply.github.com>
2023-08-21 02:49:04 +0530

    Add files via upload

---
+++ client/src/components/Orders.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Navbar from "./Navbar";
+
+export default function Orders() {
+
+    axios.defaults.withCredentials = true;
+
+
+    const [list, setList] = useState([]);
+
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/api/orders").then((response) => {
+
+
+            setList(response.data);
+
+        });
+    }, []);
+
+
+    return (
+
+        <>
+
+        <Navbar/>
+
+
+           
+
+
+            <table className="orders">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>qty</th>
+                    </tr>
+                </thead>
+
+
+
+                {
+                    list && list.length > 0
+                        ?
+                        list.map((val) => {
+
+                            return (
+
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>{val.prod_name}</td>
+                                        <td>{val.qty}</td>
+                                    </tr>
+                                    
+
+
+                                </tbody>
+
+
+
+
+                            )
+                        })
+                        :
+                        "No data available"
+
+
+                }
+
+            </table>
+
+
+
+
+        </>
+
+
+    )
+}
